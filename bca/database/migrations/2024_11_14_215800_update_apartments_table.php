a00c97b0c094dd47283c03f414525d24d19aca84
Andrej Nankov <andrejnankov@gmail.com>
2024-11-16 15:12:08 +0100

    Add docker sail

--- bca/database/migrations/2024_11_14_215800_update_apartments_table.php
+++ bca/database/migrations/2024_11_14_215800_update_apartments_table.php
@@ -1,7 +1,7 @@
 <?php
 
+
 use App\Models\Apartment;
-use App\Models\Building;
 use App\Models\Floor;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,14 +15,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table(Apartment::TABLE, function (Blueprint $table) {
-
+            // Add the foreign key column
             $table->unsignedBigInteger(Apartment::R_FLOOR_ID);
 
+            // Define the foreign key constraint
             $table->foreign(Apartment::R_FLOOR_ID)
-                ->references(Floor::TABLE)
-                ->on(Floor::TABLE)
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
+                ->references(Floor::ID) // Column in the floors table (e.g., 'id')
+                ->on(Floor::TABLE)     // The name of the floors table (e.g., 'floors')
+                ->onDelete('cascade') // Handle cascading deletes
+                ->onUpdate('cascade'); // Handle cascading updates
         });
     }
 
@@ -31,6 +32,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::drop(Floor::TABLE, Floor::R_BUILDING_ID);
+        Schema::table(Apartment::TABLE, function (Blueprint $table) {
+            // Drop the foreign key constraint and column
+            $table->dropForeign([Apartment::R_FLOOR_ID]);
+            $table->dropColumn(Apartment::R_FLOOR_ID);
+        });
     }
 };
